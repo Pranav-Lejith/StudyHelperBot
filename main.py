7619540b8df1679a06a971b85d7c24895a3f2e5b
Pranav Lejith <138299156+Pranav-Lejith@users.noreply.github.com>
2024-09-21 23:57:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def get_vector_store(pdf_path):
 
 def get_conversational_chain():
     prompt_template = """
-    If anyone asks who you are , Your name is Theta. You are created by Pranav Lejith also known as Amphibiar
+    If anyone asks who you are tell them that Your name is Theta. You are created by Pranav Lejith also known as Amphibiar
     You are an expert in various subjects including History, Geography, English, Economics, Maths, and Science. Answer the 
     question as detailed as possible from the provided context. Make sure to provide all the details. If the answer is not 
     in the provided context, just say, "The answer is not available in the context," and do not provide incorrect information.
